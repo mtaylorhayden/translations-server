58482e211d04d3995ca823697e591e3d74d1b31e
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-14 23:01:45 -0600

    guide service tests setup added

--- src/guides/guides.service.spec.ts
+++ src/guides/guides.service.spec.ts
@@ -1,12 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GuidesService } from './guides.service';
+import { EntityManager } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Guide } from './entities/guide.entity';
+import { Translation } from 'src/translations/entities/translation.entity';
+import { Sentence } from 'src/sentences/entities/sentence.entity';
 
 describe('GuidesService', () => {
   let service: GuidesService;
 
+  const mockGuideRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockTranslationRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockSentenceRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GuidesService],
+      providers: [
+        GuidesService,
+        EntityManager,
+        {
+          provide: getRepositoryToken(Guide),
+          useValue: mockGuideRepository,
+        },
+        {
+          provide: getRepositoryToken(Translation),
+          useValue: mockTranslationRepository,
+        },
+        {
+          provide: getRepositoryToken(Sentence),
+          useValue: mockSentenceRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<GuidesService>(GuidesService);
