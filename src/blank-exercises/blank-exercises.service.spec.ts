cf75260f89dbc51692a35afc86d58ca6669a7153
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 16:02:39 -0600

    blankexercisesService test passing

--- src/blank-exercises/blank-exercises.service.spec.ts
+++ src/blank-exercises/blank-exercises.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlankExercisesService } from './blank-exercises.service';
+import { EntityManager } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Workbook } from 'src/workbooks/entities/workbook.entity';
+import { BlankExercise } from './entities/blank-exercise.entity';
 
 describe('BlankExercisesService', () => {
   let service: BlankExercisesService;
 
+  const mockWorkbookRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockBlankExerciseRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlankExercisesService],
+      providers: [
+        BlankExercisesService,
+        EntityManager,
+        {
+          provide: getRepositoryToken(Workbook),
+          useValue: mockWorkbookRepository,
+        },
+        {
+          provide: getRepositoryToken(BlankExercise),
+          useValue: mockBlankExerciseRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<BlankExercisesService>(BlankExercisesService);
