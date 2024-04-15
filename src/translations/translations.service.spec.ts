e53f3975485e06b8307146113f58f69a021d9e46
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 16:11:51 -0600

    user and translation tests passing

--- src/translations/translations.service.spec.ts
+++ src/translations/translations.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TranslationsService } from './translations.service';
+import { EntityManager } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Guide } from 'src/guides/entities/guide.entity';
+import { Translation } from './entities/translation.entity';
 
 describe('TranslationsService', () => {
   let service: TranslationsService;
 
+  const mockGuideRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockTranslationRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TranslationsService],
+      providers: [
+        TranslationsService,
+        EntityManager,
+        {
+          provide: getRepositoryToken(Guide),
+          useValue: mockGuideRepository,
+        },
+        {
+          provide: getRepositoryToken(Translation),
+          useValue: mockTranslationRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TranslationsService>(TranslationsService);
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockUserRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        JwtService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
