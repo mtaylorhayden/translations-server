b81b494c24e58e31e86d8b988ca32da5d775abf4
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 15:56:15 -0600

    blankexerciseprogressController tests passing

--- src/blank-exercise-progress/blank-exercise-progress.controller.spec.ts
+++ src/blank-exercise-progress/blank-exercise-progress.controller.spec.ts
@@ -1,17 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlankExerciseProgressController } from './blank-exercise-progress.controller';
 import { BlankExerciseProgressService } from './blank-exercise-progress.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BlankExerciseProgress } from './entities/blank-exercise-progress.entity';
 
 describe('BlankExerciseProgressController', () => {
   let controller: BlankExerciseProgressController;
 
+  const mockBlankExerciseProgressRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BlankExerciseProgressController],
-      providers: [BlankExerciseProgressService],
+      providers: [
+        BlankExerciseProgressService,
+        {
+          provide: getRepositoryToken(BlankExerciseProgress),
+          useValue: mockBlankExerciseProgressRepository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<BlankExerciseProgressController>(BlankExerciseProgressController);
+    controller = module.get<BlankExerciseProgressController>(
+      BlankExerciseProgressController,
+    );
   });
 
   it('should be defined', () => {
