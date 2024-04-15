a695c8410b2eb214775ddaee5fddf53cc7ede48b
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 15:57:33 -0600

    blankexerciseprogressService tests passing

--- src/blank-exercise-progress/blank-exercise-progress.service.spec.ts
+++ src/blank-exercise-progress/blank-exercise-progress.service.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlankExerciseProgressService } from './blank-exercise-progress.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BlankExerciseProgress } from './entities/blank-exercise-progress.entity';
 
 describe('BlankExerciseProgressService', () => {
   let service: BlankExerciseProgressService;
 
+  const mockBlankExerciseProgressRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlankExerciseProgressService],
+      providers: [
+        BlankExerciseProgressService,
+        {
+          provide: getRepositoryToken(BlankExerciseProgress),
+          useValue: mockBlankExerciseProgressRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<BlankExerciseProgressService>(BlankExerciseProgressService);
+    service = module.get<BlankExerciseProgressService>(
+      BlankExerciseProgressService,
+    );
   });
 
   it('should be defined', () => {
