e46029c0a4a63aaae582d1d64c7a4d33a5f6858e
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 21:51:21 -0600

    workbookProgress tests added

--- src/workbook-progress/workbook-progress.controller.spec.ts
+++ src/workbook-progress/workbook-progress.controller.spec.ts
@@ -1,17 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkbookProgressController } from './workbook-progress.controller';
 import { WorkbookProgressService } from './workbook-progress.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { WorkbookProgress } from './entities/workbook-progress.entity';
 
 describe('WorkbookProgressController', () => {
   let controller: WorkbookProgressController;
 
+  const workbookProgressRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WorkbookProgressController],
-      providers: [WorkbookProgressService],
+      providers: [
+        WorkbookProgressService,
+        {
+          provide: getRepositoryToken(WorkbookProgress),
+          useValue: workbookProgressRepository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<WorkbookProgressController>(WorkbookProgressController);
+    controller = module.get<WorkbookProgressController>(
+      WorkbookProgressController,
+    );
   });
 
   it('should be defined', () => {
