5c12d78db833acb64959fa7b3231f1df2be9e27b
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-16 00:15:16 -0600

    pre-commit hooks

--- src/guides/guide.e2e-spec.ts
+++ src/guides/guide.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +15,18 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('GET /guides', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/guides')
       .expect(200)
-      .expect('Hello World!');
+      .expect((response) => {
+        expect(Array.isArray(response.body)).toBe(true);
+        // Optionally, check for the structure of response if you know what data to expect
+        // e.g., expect(response.body[0]).toHaveProperty('id');
+      });
   });
 });
