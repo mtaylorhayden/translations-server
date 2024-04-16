081396e6fcf5c94bd53335d1d638dfa88587b31d
Max Taylor-Hayden <nebraskamax@gmail.com>
2024-04-15 21:57:37 -0600

    workbooks tests added

--- src/workbooks/workbooks.controller.spec.ts
+++ src/workbooks/workbooks.controller.spec.ts
@@ -1,14 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkbooksController } from './workbooks.controller';
 import { WorkbooksService } from './workbooks.service';
+import { EntityManager } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Workbook } from './entities/workbook.entity';
+import { Guide } from 'src/guides/entities/guide.entity';
 
 describe('WorkbooksController', () => {
   let controller: WorkbooksController;
 
+  const mockWorkbookRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockGuidesRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WorkbooksController],
-      providers: [WorkbooksService],
+      providers: [
+        WorkbooksService,
+        EntityManager,
+        {
+          provide: getRepositoryToken(Workbook),
+          useValue: mockWorkbookRepository,
+        },
+        {
+          provide: getRepositoryToken(Guide),
+          useValue: mockGuidesRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<WorkbooksController>(WorkbooksController);
